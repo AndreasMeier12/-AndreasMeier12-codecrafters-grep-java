4a2382bebe3bcadc0d5a5bf20e4c3e2db7fedd0e
Andreas Meier <meier89@gmx.ch>
2025-08-03 06:38:26 +0200

    Set up logging or something

--- src/main/java/matcher/LiteralMatcher.java
+++ src/main/java/matcher/LiteralMatcher.java
@@ -1,7 +1,12 @@
 package matcher;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class LiteralMatcher implements IMatcher {
     private final char val;
+
+    private static Logger logger = Logger.getLogger(LiteralMatcher.class.getName());
 
     public LiteralMatcher(char val) {
         this.val = val;
@@ -14,6 +19,8 @@ public class LiteralMatcher implements IMatcher {
 
     @Override
     public MatcherResponse match(String a) {
+        logger.log(Level.INFO, "Matching literal " + val + " to value in string " + a.charAt(0));
+
         if (a.charAt(0) == this.val) {
             return new MatcherResponse(true, 1, true);
         } else
